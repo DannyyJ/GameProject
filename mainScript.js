db0f48362b0a0df32dd1485affe5497140535a30
Daniel <daniel.landqvistjacobsen@skola.taby.se>
2024-04-10 10:57:22 +0200

    Började på main menu

---
+++ mainScript.js
@@ -0,0 +1,6 @@
+window.focus;
+const SCREENWIDTH = innerWidth;
+const SCREENHEIGHT = innerHeight;
+let mainCanvas = document.getElementById("mainCanvas");
+mainCanvas.height = SCREENHEIGHT;
+mainCanvas.width=SCREENWIDTH;
