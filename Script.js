a3ee200cb13125aed2727411e39fcd2d82c7dbc2
CharlesSoder <150005047+CharlesSoder@users.noreply.github.com>
2024-04-11 11:28:20 +0200

    Shift system

--- Script.js
+++ Script.js
@@ -11,8 +11,9 @@ gameCanvas.width = SCREENWIDTH;
 let player = {
   x: 200,
   y: 200,
-  dx: 6,
-  dy: 6,
+  dx: 3,
+  dy: 3,
+  speed: 3,
   radius: 20,
   width: 200,
   height: 200,
@@ -23,6 +24,7 @@ let player = {
     FastLeft: false,
     Up: false,
     Down: false,
+    Shift: false,
   },
 };
 
@@ -33,75 +35,85 @@ let isJumping = false;
 let isMoving = false;
 
 document.addEventListener("keydown", (e) => {
+  // console.log("trycker ner", e.key);
   if (isAnimating) {
     return; // Don't allow other actions if animation is ongoing
   }
-  if (e.shiftKey) {
-    // If Shift key is pressed, increase player's movement speed
-    switch (e.key) {
-      case "a":
-        if (e.repeat) return;
-        player.direction.Left = true;
-        player.direction.FastLeft = true;
-        spriteAnimation = 2 * yOffset;
-        break;
-      case "d":
-        if (e.repeat) return;
-        player.direction.Right = true;
-        player.direction.FastRight = true;
-        spriteAnimation = 2 * yOffset;
-        break;
-      default:
-        break;
-    }
-  } else {
-    switch (e.key) {
-      case "a":
-        if (e.repeat) return;
-        player.direction.Left = true;
-        spriteAnimation = yOffset;
-        break;
-      case "d":
-        if (e.repeat) return;
-        player.direction.Right = true;
-        spriteAnimation = yOffset;
-        break;
-      case "w":
-        player.direction.Up = true;
-        isJumping = true;
-        spriteAnimation = yOffset;
-        spriteSheet = spriteSheet2;
-        break;
-      case "s":
-        if (e.repeat) return;
-        player.direction.Down = true;
-        break;
-      case "f":
-        isAnimating = true;
-        spriteAnimation = 3 * yOffset;
-        disableMovement();
-        break;
-      default:
-        break;
-    }
+  console.log(player.dx);
+  if (e.key === "Shift" && player.dx < player.speed * 2) {
+    player.dx *= 2;
+  }
+  // if (player.direction.Shift == true) {
+  //   console.log("Håller ner shift och trycker", e.key);
+  // If Shift key is pressed, increase player's movement speed
+  // console.log(e.key);
+
+  switch (e.key) {
+    case "a":
+      if (e.repeat) return;
+      player.direction.Left = true;
+      spriteAnimation = yOffset;
+      break;
+    case "d":
+      // console.log("Fastright is first:", player.direction.FastRight);
+      if (e.repeat) return;
+      player.direction.Right = true;
+      spriteAnimation = yOffset;
+      break;
+    case "w":
+      player.direction.Up = true;
+      isJumping = true;
+      spriteAnimation = yOffset;
+      spriteSheet = spriteSheet2;
+      break;
+    case "s":
+      if (e.repeat) return;
+      player.direction.Down = true;
+      break;
+    case "f":
+      isAnimating = true;
+      spriteAnimation = 3 * yOffset;
+      disableMovement();
+      break;
+    default:
+      break;
   }
 });
 
 document.addEventListener("keyup", (e) => {
+  // if (e.key == "Shift") {
+  //   player.direction.Shift = false;
+  // }
+
+  // if (player.direction.Shift == true) {
+  //   disableMovement();
+  // }
+  if (e.key === "Shift" && player.dx > player.speed) {
+    player.dx /= 2;
+  }
+  if (player.direction.Left == true) {
+    spriteAnimation = yOffset;
+  }
+  if (player.direction.Right == true) {
+    spriteAnimation = yOffset;
+  }
   switch (e.key) {
+    case "Shift":
+      player.direction.Shift = false;
+      player.direction.FastLeft = false;
+      player.direction.FastRight = false;
+      player.direction.Right = false;
+      player.direction.Left = false;
+      spriteAnimation = 0;
+      player.dx = player.speed;
+      break;
     case "a":
       player.direction.Left = false;
       spriteAnimation = 0;
-      if (e.shiftKey) {
-        player.direction.FastLeft = false;
-      }
       break;
     case "d":
       player.direction.Right = false;
       spriteAnimation = 0;
-      if (e.shiftKey) {
-        player.direction.FastRight = false;
-      }
       break;
     case "w":
       player.direction.Up = false;
@@ -120,8 +132,12 @@ document.addEventListener("keyup", (e) => {
 });
 
 function disableMovement() {
-  player.direction.Left = false;
-  player.direction.Right = false;
+  if (!player.direction.Shift) {
+    player.direction.Left = false;
+    player.direction.FastLeft = false;
+    player.direction.Right = false;
+    player.direction.FastRight = false;
+  }
   player.direction.Up = false;
   player.direction.Down = false;
 }
@@ -213,6 +229,54 @@ function drawPlayer(spriteSheet, heightDivider, widthDivider, frameIndex) {
     (spriteSheet.width / widthDivider) * scale
   );
 }
+function updatePlayerPosition() {
+  if (player.direction.Right && player.dx > player.speed) {
+    spriteAnimation = 2 * yOffset;
+  }
+  if (player.direction.Left && player.dx > player.speed) {
+    spriteSheet = spriteSheet2;
+    spriteAnimation = yOffset;
+  }
+  if (player.dx <= player.speed) {
+    spriteSheet = spriteSheet1;
+  }
+  if (
+    player.direction.FastRight &&
+    player.x + player.width < gameCanvas.width
+  ) {
+    player.x += player.dx * 3;
+    isMoving = true;
+  } else if (
+    player.direction.Right &&
+    player.x + player.width < gameCanvas.width
+  ) {
+    player.x += player.dx;
+  } else if (
+    player.direction.Left &&
+    player.direction.FastLeft &&
+    player.x > 0
+  ) {
+    player.x -= player.dx * 3;
+    isMoving = true;
+  } else if (player.direction.Left && player.x > 0) {
+    player.x -= player.dx;
+  } else {
+    isMoving = false;
+  }
+
+  if (
+    player.direction.Up &&
+    player.y + player.height >= gameCanvas.height - 2
+  ) {
+    player.dy -= 10; // Jump height
+    isJumping = true;
+  } else if (
+    player.direction.Down &&
+    player.y + player.height < gameCanvas.height
+  ) {
+    player.y += player.dy;
+  }
+}
 
 function game() {
   requestAnimationFrame(game); // Run gameloop recursively
@@ -223,44 +287,6 @@ function game() {
   //Hantera gravity och så att han inte faller genom marken
   player.y += player.dy;
   player.dy += Gravity;
-
-  if (
-    player.direction.Right == true &&
-    player.direction.FastRight == true &&
-    player.x + player.width < gameCanvas.width
-  ) {
-    player.x += player.dx * 1.5;
-    isMoving = true;
-  } else if (
-    player.direction.Right == true &&
-    player.x + player.width < gameCanvas.width
-  ) {
-    player.x += player.dx;
-  } else if (
-    player.direction.Left == true &&
-    player.direction.FastLeft == true &&
-    player.x > 0
-  ) {
-    player.x -= player.dx * 1.5;
-    isMoving = true;
-  } else if (player.direction.Left == true && player.x > 0) {
-    player.x -= player.dx;
-  } else {
-    isMoving = false;
-  }
-  if (
-    player.direction.Up == true &&
-    player.y + player.height >= gameCanvas.height - 2
-  ) {
-    //Jump height
-    player.dy -= 10;
-    isJumping = true;
-  } else if (
-    player.direction.Down == true &&
-    player.y + player.height < gameCanvas.height
-  ) {
-    player.y += player.dy;
-  }
 
   if (player.y + player.height >= gameCanvas.height - 2) {
     isJumping = false; // Player is no longer jumping
@@ -277,9 +303,9 @@ function game() {
     spriteSheet = spriteSheet2;
     spriteAnimation = 0;
   }
+  updatePlayerPosition();
 }
 
 // -------------------------------------
 // ------------ Start game ------------
 game();
-
